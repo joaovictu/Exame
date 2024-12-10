c245f42ace2f02b1cf3af24e416166f8795d35cb
João <ojoaovictorcs@gmail.com>
2024-12-10 20:13:46 -0300

    Configuração Inicial do Servidor

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bem-vindo à Loja de Pranchas!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
